2f8d5adc689ea72674cfbe749cd96a510081253b
LukeChamley <lkchamley@gmail.com>
2022-08-15 15:45:29 -0600

    commit tree e001fe49042283b27d6771676805ee3e187ce47c

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your code here
+
+
+
+/*const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      dogName: "Byron",
+      dogBreed: "Poodle",
+    }),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject);
+*/
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         name: joe,
+//         email: joemail.com
+//     }),
+//};
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        }),
+    })
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (object) {
+        document.body.innerHTML=object["id"];
+      })
+    .catch(function (error) {
+        document.body.innerHTML = error.message
+  }) 
+}
